c530290f4e6feece403acc0a9109e7b696ddd1c6
Serene H <keroserene+git@gmail.com>
2014-02-05 21:49:03 -0500

    consolidate peers into RtcToNet.Server

--- src/rtc-to-net/rtc-to-net.ts
+++ src/rtc-to-net/rtc-to-net.ts
@@ -6,24 +6,34 @@ declare var freedom:any;
 
 /// <reference path='netclient.ts' />
 
-module RTCToNet {
+
+module RtcToNet {
 
   var FCore = freedom.core();
 
+  interface PeerConnection {
+    // TODO: Make these more typeful and probably shove into freedom.
+    on:(event:string,f:any)=>void;
+    setup:any;
+    shutdown:any;
+    send:any;
+    closeDataChannel:any
+  }
+
   /**
-   * RTCToNet.Peer
+   * RtcToNet.Peer
    *
    * Serves requests from WebRTC peer connections.
    */
   export class Peer {
 
-    sctpPc:any;
-    netClients:{[channelLabel:string]:Net.Client} = {};
-    signallingChannel:any = null;
-    messageQueue:any[];
+    private signallingChannel:any = null;
+    private sctpPc:PeerConnection;
+    private netClients:{[channelLabel:string]:Net.Client} = {};
+    private messageQueue:any[];
 
     constructor (public peerId) {
-      console.log('New Peer created ' + peerId);
+      console.log('New RtcToNet.Peer: ' + peerId);
       this.netClients = {};
       this.messageQueue = [];
 
@@ -31,30 +41,30 @@ module RTCToNet {
       this.sctpPc = freedom['core.sctp-peerconnection']();
       this.sctpPc.on('onReceived', (message) => {
         // console.log("Server got message: " + JSON.stringify(message));
-        var cL = message.channelLabel;
-        if (!cL) {
-          console.error("Message received but missing channelLabel. Msg: " +
-              JSON.stringify(message));
+        var label = message.channelLabel;
+        if (!label) {
+          console.error('Message received but missing channelLabel. Msg: ' +
+                        JSON.stringify(message));
           return;
         }
         if (message.text) {
           // Text from the peer is used to set a new destination request.
           // Assumes "message.text" is a json of form:
           // { host: string, port: number }
-          this.netClients[cL] = new Net.Client(
-              this.sendDataToPeer_.bind(this, cL),
-              this.closeClient_.bind(this, cL),
+          this.netClients[label] = new Net.Client(
+              this.sendDataToPeer_.bind(this, label),
+              this.closeClient_.bind(this, label),
               JSON.parse(message.text));
         } else if (message.buffer) {
-          if(!(cL in this.netClients)) {
-            console.error("Message received for non-existent channel. Msg: " +
+          if(!(label in this.netClients)) {
+            console.error('Message received for non-existent channel. Msg: ' +
               JSON.stringify(message));
             return;
           }
           // Buffer from the peer is data for the destination.
-          this.netClients[cL].send(message.buffer);
+          this.netClients[label].send(message.buffer);
         } else {
-          console.error("Message received but missing valid data field. Msg: " +
+          console.error('Message received but missing valid data field. Msg: ' +
               JSON.stringify(message));
         }
       });
@@ -86,7 +96,17 @@ module RTCToNet {
           });
         });
       });
-      console.log('_initPeer(' + peerId + ') complete.');
+    }
+
+    // Send data over the peer's signalling channel, or queue if not ready.
+    public sendSignal = (data:any) => {
+      if (!this.signallingChannel) {
+        console.log('RtcToNet:Peer[' + this.peerId + '] signallingChannel ' +
+                    'not yet ready. Adding to queue... ');
+        this.messageQueue.push(data);
+        return;
+      }
+      this.signallingChannel.emit('message', data);
     }
 
     // Close peer connection and all tcp sockets open for this peer.
@@ -119,15 +139,14 @@ module RTCToNet {
         console.log("server.js: closePeer_: got a bad label.  Stack trace: " +
             err.stack); */
       }
-      // console.log("Peer DataChannel " + labelnm + " closed.");
       if (close_client) {
         // console.log("Peer DataChannel " + labelnm + " closing NetClient socket..");
         // var num_clients_found = 0;
-        // TODO: Why is this checking the netclients on other peers?
-        // for (var i in _peers) {
-          // if (_peers[i].netClients[label]) {
-            // _peers[i].netClients[label].close();
-            // delete _peers[i].netClients[label];
+        // TODO: Do other peers actually share netclients with the same channel?
+        // for (var i in peers_) {
+          // if (peers_[i].netClients[label]) {
+            // peers_[i].netClients[label].close();
+            // delete peers_[i].netClients[label];
             // num_clients_found++;
           // }
         // }
@@ -138,75 +157,65 @@ module RTCToNet {
       }
     }
 
-  }  // class RTCToNet.Peer
+  }  // class RtcToNet.Peer
+
 
   export class Server {
-  }  // class RTCToNet.Server
 
-}  // module RTCToNet
+    // Maintain a mapping of peerIds to peers.
+    private peers_:{[peerId:string]:Peer} = {};
+
+    /**
+     * signal.peerId : of the peer sending the signal.
+     * signal.data : message body from signalling channel, typically SDP headers.
+     */
+    public handleSignal = (signal) => {
+      console.log('server handleSignalFromPeer:' + JSON.stringify(signal));
+      if (!signal.peerId) {
+        console.error('RtcToNet.Server: signal received with no peerId!');
+        return;
+      }
+      // TODO: Check for access control?
+      console.log('sending to transport: ' + JSON.stringify(signal.data));
+      var peer = this.fetchPeer_(signal.peerId);
+        // Send response to peer.
+      peer.sendSignal(signal.data);
+    }
+
+    private fetchPeer_(peerId) {
+      var peer = this.peers_[peerId];
+      if (peer) {
+        return peer;
+      }
+      // Create peer for this id if necessary.
+      console.log('rtc-to-net.ts: new peer: ' + peerId);
+      this.peers_[peerId] = peer = new RtcToNet.Peer(peerId);
+      return peer;
+    }
+
+    /** Close all peers on this server. */
+    public reset = () => {
+      for (var contact in this.peers_) {
+        this.peers_[contact].close();
+        delete this.peers_[contact];
+      }
+      this.peers_ = {};
+    }
+
+  }  // class RtcToNet.Server
+
+}  // module RtcToNet
+
 
 function initServer() {
-
-  // Maintain a mapping of peerIds to peers.
-  var _peers:{[peerId:string]:any} = {};
-  var _active:boolean = true;  // this variable can only make things worse.
-
-  function resetServer() {
-    for (var contact in _peers) {
-      _peers[contact].close();
-      delete _peers[contact];
-    }
-    _peers = {};
-    _active = false;
-  }
-
-  function initPeer(peerId) {
-    console.log('rtc-to-net.ts: initPeer(' + peerId + ')'); //  _peers = " +
-    if (!_peers[peerId]) {
-      _peers[peerId] = new RTCToNet.Peer(peerId);
-    }
-  }
-
-  freedom.on('start', function() {
-    console.log("Starting server.");
-    resetServer();
-    _active = true;
+  var server = new RtcToNet.Server();
+  freedom.on('start', () => {
+    console.log('Starting server.');
+    server.reset();  // Fresh start!
   });
-
-  // msg.peerId : string of the clientId for the peer being sent a message.
-  // msg.data : message body received peerId signalling channel, typically
-  //            contains SDP headers.
-  //
-  freedom.on('handleSignalFromPeer', function(msg) {
-    console.log("server handleSignalFromPeer:" + JSON.stringify(msg));
-    if (!_active) {
-      console.log("server is not active, returning");
-      return;
-    }
-    if (!msg.peerId) {
-      console.error('No peer ID provided!.')
-      return;
-    }
-    // TODO: Check for access control?
-    console.log("sending to transport: " + JSON.stringify(msg.data));
-    // Make a peer for this id if it doesn't already exist.
-    if (!_peers[msg.peerId]) {
-      initPeer(msg.peerId);
-    }
-    if (_peers[msg.peerId].signallingChannel){
-      // Send response to peer.
-      console.log('SENDING!!!!! ' + JSON.stringify(msg.data));
-      //window.tmp = _peers[msg.peerId];
-      _peers[msg.peerId].signallingChannel.emit('message', msg.data);
-    } else {
-      console.log('signallingChannel not yet ready. Adding to queue... ' + msg.peerId + ' ... ' + _peers);
-      _peers[msg.peerId].messageQueue.push(msg.data);
-    }
-  });
-
-  freedom.on('stop', resetServer);
+  freedom.on('handleSignalFromPeer', server.handleSignal);
+  freedom.on('stop', server.reset);
   freedom.emit('ready', {});
-
   console.log('socks-rtc Server initialized.');
 }
 
