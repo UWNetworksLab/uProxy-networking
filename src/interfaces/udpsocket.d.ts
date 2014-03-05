8ae405dbfd87bac9aee421f2a890e71c26882ed0
yangoon <yangoon@github.com>
2014-03-05 10:44:06 -0500

    add SocketInfo interface

--- src/interfaces/udpsocket.d.ts
+++ src/interfaces/udpsocket.d.ts
@@ -4,6 +4,15 @@
 
 declare module UdpSocket {
 
+  // Type for the chrome.socket.getInfo callback:
+  //   https://developer.chrome.com/apps/sockets_udp#type-SocketInfo
+  // This is also the type returned by getInfo().
+  export interface SocketInfo {
+    // Note that there are other fields but these are the ones we care about.
+    localAddress:string;
+    localPort:number;
+  }
+
   export interface API {
     bind:any;
     sendTo:any;
