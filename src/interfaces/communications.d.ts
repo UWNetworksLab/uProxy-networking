449d66c97968f4a6f8fad6f5242e979efad24950
yangoon <yangoon@github.com>
2014-03-19 18:16:37 -0400

    define messages for the control channel

--- src/interfaces/communications.d.ts
+++ src/interfaces/communications.d.ts
@@ -1,22 +1,38 @@
 // Types for communications between socks-to-rtc and rtc-to-net.
 
 declare module Channel {
+  // "Top-level" message for the control channel.
+  export interface Command {
+    // Name of message, e.g. NetConnectRequest.
+    type:string;
+    // Datachannel with which this message is associated.
+    tag?:string;
+    // JSON-encoded message, e.g. NetConnectRequest.
+    data?:string;
+  }
 
-  export interface Message {
-    channelLabel:string;
-    text?:string;
-    buffer?:ArrayBuffer;
+  // Sent to request a connection be established with a remote server.
+  export interface NetConnectRequest {
+    // 'tcp' or 'udp'.
+    protocol:string;
+    // Destination address and port.
+    address:string;
+    port:number;
+  }
+
+  export interface NetConnectResponse {
+    // Address and port on which we have made the connection to the
+    // remote server, or both undefined if the connection could not be
+    // made.
+    address?:string;
+    port?:number;
   }
 
   // Should be returned after tieing a data-channel with SOCKS, back to form an
   // endpoint response to the local TCP server.
-  interface EndpointInfo {
+  export interface EndpointInfo {
     ipAddrString:string;
     port:number;
-  }
-
-  interface CloseData {
-    channelId:string;
   }
 
 }  // module Channel
