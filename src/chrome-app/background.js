db89b0c01cf786571d92f96f5d0a64c55d8acf9e
Raymond Cheng <ryscheng@cs.washington.edu>
2014-07-22 14:23:54 -0700

    fix mistake in package.json, turn off freedom.js debug messages

--- src/chrome-app/background.js
+++ src/chrome-app/background.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 script.setAttribute('data-manifest', 'socks_rtc.json');
 //script.setAttribute('data-manifest', 'test/tcp_echo_server.json');
-script.textContent = '{ "debug": true }';
+script.textContent = '{ "debug": false }';
 script.src = 'freedom-for-chrome.js';
 document.head.appendChild(script);
