ccbf9b315ba6f9ab1885c7c5c9aae3ea52166420
Trevor Johnston <trevj@google.com>
2014-10-10 12:33:51 -0400

    mock providePromises for churn test

--- src/churn/mocks.js
+++ src/churn/mocks.js
@@ -2,3 +2,5 @@
 // We do this in a non-TypeScript file because the ambient module declaration
 // prevents us creating any variable called freedom in TypeScript-land.
 var freedom = jasmine.createSpyObj('freedom', ['core.log']);
+freedom.churn = jasmine.createSpy().and.returnValue(
+    jasmine.createSpyObj('churn', ['providePromises']));
