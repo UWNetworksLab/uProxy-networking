05f605e3f376c920620b9d47470832976b92ce92
Trevor Johnston <trevj@google.com>
2014-10-14 17:08:32 -0400

    add onceStarted to the .d.ts

--- src/socks-to-rtc/socks-to-rtc.d.ts
+++ src/socks-to-rtc/socks-to-rtc.d.ts
@@ -1,39 +1,36 @@
-/// <reference path="../socks-common/socks-headers.d.ts" />
-/// <reference path="../freedom/coreproviders/uproxylogging.d.ts" />
 /// <reference path="../freedom/coreproviders/uproxypeerconnection.d.ts" />
-/// <reference path="../freedom/typings/freedom.d.ts" />
 /// <reference path="../handler/queue.d.ts" />
 /// <reference path="../networking-typings/communications.d.ts" />
-/// <reference path="../churn/churn.d.ts" />
 /// <reference path="../webrtc/datachannel.d.ts" />
 /// <reference path="../webrtc/peerconnection.d.ts" />
 /// <reference path="../tcp/tcp.d.ts" />
 /// <reference path="../third_party/typings/es6-promise/es6-promise.d.ts" />
 declare module SocksToRtc {
-    class SocksToRtc {
-        onceStarted: () => Promise<void>;
-        onceReady: Promise<Net.Endpoint>;
-        onceStopped: () => Promise<void>;
-        signalsForPeer: Handler.Queue<WebRtc.SignallingMessage, void>;
-        bytesReceivedFromPeer: Handler.Queue<number, void>;
-        bytesSentToPeer: Handler.Queue<number, void>;
-        constructor(endpoint?: Net.Endpoint, pcConfig?: WebRtc.PeerConnectionConfig, obfuscate?: boolean);
-        configure(tcpServer: Tcp.Server, peerconnection: freedom_UproxyPeerConnection.Pc): void;
-        makeOnceStarted(serverReady: Promise<any>, peerconnectionReady: Promise<any>): void;
-        makeOnceStopped(serverTerminated: Promise<any>, peerconnectionTerminated: Promise<any>): void;
-        stop: () => Promise<void>;
-        makeTcpToRtcSession: (tcpConnection: Tcp.Connection) => void;
-        handleSignalFromPeer: (signal: WebRtc.SignallingMessage) => void;
-        toString: () => string;
-    }
-    class Session {
-        onceReady: Promise<Net.Endpoint>;
-        onceClosed: Promise<void>;
-        constructor(tcpConnection: Tcp.Connection, peerConnection_: freedom_UproxyPeerConnection.Pc, bytesReceivedFromPeer: Handler.Queue<number, void>, bytesSentToPeer: Handler.Queue<number, void>);
-        longId: () => string;
-        close: () => Promise<void>;
-        handleDataFromPeer: (data: WebRtc.Data) => void;
-        channelLabel: () => string;
-        toString: () => string;
-    }
+  class SocksToRtc {
+    constructor(endpoint:Net.Endpoint,
+                pcConfig:WebRtc.PeerConnectionConfig,
+                obfuscate:boolean);
+    public stop :() => Promise<void>;
+    public onceStarted: () => Promise<void>;
+    public onceReady :Promise<Net.Endpoint>;
+    public isStopped :() => boolean;
+    public onceStopped :() => Promise<void>;
+    public signalsForPeer :Handler.Queue<WebRtc.SignallingMessage, void>;
+    public bytesReceivedFromPeer :Handler.Queue<number, void>;
+    public bytesSentToPeer :Handler.Queue<number, void>;
+    public handleSignalFromPeer :(signal: WebRtc.SignallingMessage) => void;
+    public toString :() => string;
+  }
+  class Session {
+    constructor(tcpConnection:Tcp.Connection,
+                peerConnection_:freedom_UproxyPeerConnection.Pc);
+    public tcpConnection :Tcp.Connection;
+    public onceReady :Promise<Net.Endpoint>;
+    public onceClosed :Promise<void>;
+    public longId :() => string;
+    public close :() => Promise<void>;
+    public handleDataFromPeer :(data:WebRtc.Data) => void;
+    public channelLabel :() => string;
+    public toString :() => string;
+  }
 }
