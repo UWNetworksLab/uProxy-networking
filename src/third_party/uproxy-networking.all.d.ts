e02783817e2441bd6eb0ec9c4e923bc76e22e0af
Lally Singh <lally@google.com>
2014-10-16 17:01:34 -0400

    Cleanup after uproxy-lib changes

--- src/third_party/uproxy-networking.all.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="request/request.d.ts" />
-/// <reference path="socks5-http-client/socks5-http-client.d.ts" />
-/// <reference path="socks5-http-client/lib/Agent/Agent.d.ts" />
-
