695b5ef7b935c721d4c262da25bc8039c479e92e
Trevor Johnston <trevj@google.com>
2014-10-29 11:22:42 -0400

    actually add tcp integration test

---
+++ src/tcp/tcp.integration.spec.ts
@@ -0,0 +1,9 @@
+/// <reference path='tcp.d.ts' />
+
+/// <reference path='../third_party/typings/jasmine/jasmine.d.ts' />
+
+describe('TCP wrapper', function() {
+  it('runs', () => {
+    expect(true).toEqual(true);
+  });
+});
