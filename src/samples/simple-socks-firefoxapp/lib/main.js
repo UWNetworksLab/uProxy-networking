92705890e7df751da3d8ef633f7784a10a5d69db
Trevor Johnston <trevj@google.com>
2014-11-11 14:52:18 -0500

    update simple socks firefox for freedomjs 0.6

--- src/samples/simple-socks-firefoxapp/lib/main.js
+++ src/samples/simple-socks-firefoxapp/lib/main.js
@@ -2,8 +2,11 @@ const {Cu} = require("chrome");
 var self = require("sdk/self");
 var {setTimeout} = require("sdk/timers");
 
-Cu.import(self.data.url("lib/freedom/freedom-for-firefox-for-uproxy.jsm"));
+Cu.import(self.data.url("lib/freedom/freedom-for-firefox.jsm"));
 
 var manifest = self.data.url("lib/simple-socks/freedom-module.json");
-
-var freedom = setupFreedom(manifest, { 'debug': 'log' });
+freedom(manifest, { 'debug': 'log' }).then(function(interface) {
+  var simpleSocks = interface();
+}, function() {
+  console.error('could not load freedom');
+});
