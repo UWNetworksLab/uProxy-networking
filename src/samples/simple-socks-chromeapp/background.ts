a9f23a6c5e64109e7cc22d62a429bd6f549f8be8
Trevor Johnston <trevj@google.com>
2014-11-17 12:30:41 -0500

    not freedom debug log

--- src/samples/simple-socks-chromeapp/background.ts
+++ src/samples/simple-socks-chromeapp/background.ts
@@ -5,7 +5,7 @@ script.src = 'lib/freedom/freedom-for-chrome.js';
 document.head.appendChild(script);
 
 script.onload = () => {
-  freedom('lib/simple-socks/freedom-module.json', { 'debug': 'debug' }).then(function(interface:any) {
+  freedom('lib/simple-socks/freedom-module.json', { 'debug': 'log' }).then(function(interface:any) {
     var simpleSocks :any = interface();
   }, (e:Error) => {
     console.error('could not load freedom: ' + e.message);
