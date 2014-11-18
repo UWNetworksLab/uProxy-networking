cc003b3da78fb20df94623b5936441fb78131d21
Trevor Johnston <trevj@google.com>
2014-11-18 12:48:29 -0500

    too much logging

--- src/samples/simple-churn-chat-chromeapp/main.ts
+++ src/samples/simple-churn-chat-chromeapp/main.ts
@@ -8,7 +8,7 @@ var sendAreaB = <HTMLInputElement>document.getElementById("sendAreaB");
 var receiveAreaA = <HTMLInputElement>document.getElementById("receiveAreaA");
 var receiveAreaB = <HTMLInputElement>document.getElementById("receiveAreaB");
 
-freedom('freedom-module.json', { 'debug': 'debug' }).then(function(interface:any) {
+freedom('freedom-module.json', { 'debug': 'log' }).then(function(interface:any) {
   var simpleChurnChat :any = interface();
 
   simpleChurnChat.on('ready', function() {
