81b196d9990ea9816b7541910faee09b0e91b44b
Trevor Johnston <trevj@google.com>
2015-01-16 17:52:59 -0500

    update simple socks for loggingProvider

--- src/samples/simple-socks-chromeapp/background.ts
+++ src/samples/simple-socks-chromeapp/background.ts
@@ -5,7 +5,10 @@ script.src = 'lib/freedom/freedom-for-chrome.js';
 document.head.appendChild(script);
 
 script.onload = () => {
-  freedom('lib/simple-socks/freedom-module.json', { 'debug': 'log' }).then(function(interface:any) {
+  freedom('lib/simple-socks/freedom-module.json', {
+      'logger': 'lib/loggingprovider/loggingprovider.json',
+      'debug': 'log'
+  }).then(function(interface:any) {
     var simpleSocks :any = interface();
   }, (e:Error) => {
     console.error('could not load freedom: ' + e.message);
--- src/simple-socks/freedom-module.ts
+++ src/simple-socks/freedom-module.ts
@@ -10,7 +10,7 @@
 // you're debugging. Since the proxy outputs quite a lot of messages,
 // show only warnings by default from the rest of the system.
 // Note that the proxy is extremely slow in debug (D) mode.
-Logging.setConsoleFilter([
+freedom['loggingprovider']().setConsoleFilter([
     '*:I',
     'SocksToRtc:I',
     'RtcToNet:I']);
