1e7295ce471f7df07cce2b9045a1cfb7d0407302
Lally Singh <lally@google.com>
2015-01-20 11:39:53 -0500

    Added comment describing the sleep.

--- src/samples/simple-socks-chromeapp/background.ts
+++ src/samples/simple-socks-chromeapp/background.ts
@@ -5,10 +5,13 @@ script.src = 'lib/freedom/freedom-for-chrome.js';
 document.head.appendChild(script);
 
 script.onload = () => {
+  // Keep a background timeout running continuously, to prevent chrome from
+  // putting the app to sleep.
   function keepAlive() {
     setTimeout(keepAlive, 5000);
   }
   keepAlive();
+
   freedom('lib/simple-socks/freedom-module.json', { 'debug': 'log' }).then(function(interface:any) {
     var simpleSocks :any = interface();
   }, (e:Error) => {
