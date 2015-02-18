4ec409bab399f67e26b76731bfc524cc50af1129
Trevor Johnston <trevj@google.com>
2015-02-18 12:11:54 -0500

    just make everything info for now

--- src/simple-socks/freedom-module.ts
+++ src/simple-socks/freedom-module.ts
@@ -10,10 +10,7 @@
 // you're debugging. Since the proxy outputs quite a lot of messages,
 // show only warnings by default from the rest of the system.
 // Note that the proxy is extremely slow in debug (D) mode.
-freedom['loggingprovider']().setConsoleFilter([
-    '*:I',
-    'SocksToRtc:I',
-    'RtcToNet:I']);
+freedom['loggingprovider']().setConsoleFilter(['*:I']);
 
 var log :Logging.Log = new Logging.Log('simple-socks');
 
