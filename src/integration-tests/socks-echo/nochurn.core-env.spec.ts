5a346185002b75219f2e96bd489e604d7156a1af
Lucas Dixon <ldixon@google.com>
2015-02-24 10:37:09 -0500

    filename cleanup

--- src/integration-tests/socks-echo/nochurn.core-env.spec.ts
+++ src/integration-tests/socks-echo/nochurn.core-env.spec.ts
@@ -1,4 +1,4 @@
-import echotest = require('socks-echo-base-spec.core-env');
+import echotest = require('base-spec.core-env');
 
 describe('proxy integration tests', function() {
   echotest.socksEchoTestDescription(false);
