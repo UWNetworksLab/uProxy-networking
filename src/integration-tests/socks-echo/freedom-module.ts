bad6f2753fc885d0b5ac5d9584d38e149b5ada52
Ben Schwartz <bemasc@uproxy.org>
2015-04-13 10:44:27 -0400

    Rename ProxyIntegrationTestClass to AbstractProxyIntegrationTest

--- src/integration-tests/socks-echo/freedom-module.ts
+++ src/integration-tests/socks-echo/freedom-module.ts
@@ -1,12 +1,12 @@
 /// <reference path='../../../../third_party/freedom-typings/freedom-common.d.ts' />
 /// <reference path='../../../../third_party/freedom-typings/freedom-module-env.d.ts' />
 
-import ProxyIntegrationTestClass = require('./proxy-integration-test');
+import AbstractProxyIntegrationTest = require('./proxy-integration-test');
 
 // Example of how to set custom logging level: we set everything to debug for
 // testing echo server.
 freedom['loggingcontroller']().setConsoleFilter(['*:D']);
 
 if (typeof freedom !== 'undefined') {
-  freedom().providePromises(ProxyIntegrationTestClass);
+  freedom().providePromises(AbstractProxyIntegrationTest);
 }
