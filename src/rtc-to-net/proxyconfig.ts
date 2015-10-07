bf3806c5cced666d6e637390b20876c7fb4fb8c9
Trevor Johnston <trevj@google.com>
2015-10-07 14:28:56 -0400

    remove code, update README to point to uproxy-lib

--- src/rtc-to-net/proxyconfig.ts
+++
@@ -1,8 +0,0 @@
-
-interface ProxyConfig {
-  // If |allowNonUnicast === false| then any proxy attempt that results
-  // in a non-unicast (e.g. local network) address will fail.
-  allowNonUnicast :boolean;
-}
-
-export = ProxyConfig;
--- src/samples/copypaste-socks-chromeapp/i18n-util.types.ts
+++
@@ -1,6 +0,0 @@
-interface I18nUtil {
-  translateStrings(node:Element) : void;
-  changeLanguage(language:string) : void;
-  getBrowserLanguage() : string;
-}
-export = I18nUtil;
